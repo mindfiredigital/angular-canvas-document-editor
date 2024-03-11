80c75e931f704a9f4a8aaf4729e15d6209d13832
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2024-03-11 13:47:43 +0530

    feat: setup angular projects

---
+++ document-editor/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MyLibComponent } from 'my-lib';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MyLibComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass',
+})
+export class AppComponent {
+  title = 'document-editor';
+}
