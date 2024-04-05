7cba18e50854f46b741ce1ca3bd85699df76ca57
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2024-04-05 13:30:40 +0530

    feat: custom dropdown

---
+++ projects/my-lib/src/lib/components/dropdown/dropdown.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Dropdown } from '../../interface/dropdown';
+@Component({
+  selector: 'lib-dropdown',
+  standalone: true,
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.css',
+})
+export class DropdownComponent {
+  @Input() name: string | undefined;
+  @Input() items: Dropdown[] | undefined;
+}
---
+++ projects/my-lib/src/lib/interface/dropdown.ts
@@ -0,0 +1,4 @@
+export interface Dropdown {
+  value: string;
+  viewValue: string;
+}
