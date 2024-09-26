bfefb8bc46fc875e163b83082ffa090c86268f54
anilkumarswain140 <142868838+anilkumarswain140@users.noreply.github.com>
2024-09-26 16:24:38 +0530

    feat: add all tools undo,redo,image upload,margin modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CanvasEditorComponent } from './components/canvas-editor/canvas-editor.component';
+import { EditorFooterComponent } from './components/editor-footer/editor-footer.component';
+import { EditorToolbarComponent } from './components/editor-toolbar/editor-toolbar.component';
+// import { MyLibComponent } from '../../projects/my-lib/src/lib/my-lib.component';
 // import { RouterOutlet } from '@angular/router';
-import { MyLibComponent } from 'my-lib';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-lib-app',
   standalone: true,
-  imports: [MyLibComponent],
+  imports: [    EditorFooterComponent,
+    CanvasEditorComponent,
+    EditorToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { documentReducer } from './store/document.reducer';
+import { provideStore } from '@ngrx/store';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync('noop')],
+  providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync('noop'), provideAnimationsAsync(),provideStore({ document: documentReducer }),],
 };
