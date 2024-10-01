913d5fc10ae98f603b39f7b260bb958ad5c76d55
anilkumarswain140 <142868838+anilkumarswain140@users.noreply.github.com>
2024-10-01 16:41:20 +0530

    feat: add typescript comilation changes

--- projects/my-canvas-lib/src/public-api.ts
+++ projects/my-canvas-lib/src/public-api.ts
@@ -5,5 +5,3 @@
 export * from './lib/my-lib.component';
 export * from './lib/services/document.service';
 export * from './lib/my-lib.module';
-
-
---
+++ projects/my-canvas-lib/src/typings/canvas-editor.d.ts
@@ -0,0 +1,12 @@
+declare module '@mindfiredigital/canvas-editor' {
+    export const RowFlex: any; // Replace with actual type if known
+    export const ListType: any; // Replace with actual type if known
+    export interface IRangeStyle {
+      // Define properties based on your usage
+    }
+    export interface DOMEventHandlers {
+      // Define properties based on your usage
+    }
+    // Add any other exports you need to declare here
+  }
+  
